6e0f2c28b83e069f70558cb6f20748da88eb737a
naino <Hasnainkhan21>
2024-09-08 11:05:47 +0500

    form styled

---
+++ index.js
@@ -0,0 +1,27 @@
+const questions = [
+    {
+        question: "What is the capital of France?",
+        choices: ["Berlin", "Madrid", "Paris", "Lisbon"],
+        correctAnswer: "Paris"
+    },
+    {
+        question: "Which planet is known as the Red Planet?",
+        choices: ["Earth", "Mars", "Jupiter", "Saturn"],
+        correctAnswer: "Mars"
+    },
+    {
+        question: "Who wrote 'To Kill a Mockingbird'?",
+        choices: ["Harper Lee", "Mark Twain", "J.K. Rowling", "Ernest Hemingway"],
+        correctAnswer: "Harper Lee"
+    },
+    {
+        question: "What is the largest ocean on Earth?",
+        choices: ["Atlantic Ocean", "Indian Ocean", "Southern Ocean", "Pacific Ocean"],
+        correctAnswer: "Pacific Ocean"
+    },
+    {
+        question: "Which element has the chemical symbol 'O'?",
+        choices: ["Gold", "Oxygen", "Silver", "Iron"],
+        correctAnswer: "Oxygen"
+    }
+];
